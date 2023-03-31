d1a0a05d455a4300e43db86eaf173df87e9c9a41
widelc <79269052+widelc@users.noreply.github.com>
2023-03-31 18:37:02 -0400

    Add files via upload

--- Function/f_plot_pdf.R
+++ Function/f_plot_pdf.R
@@ -1,73 +1,90 @@
 # Import libraries
 library("here")
+library("ggplot2")
 
-f_plot_pdf <- function(x, title, VaR, y_max) {
+f_plot_pdf <- function(result, model_name) {
   
   ### Plots the density function of x
   
   #  INPUTS
-  #   x      : [vector] (T x 1) of data to plot
-  #   title  : [string] of the title of the graph
-  #   VaR    : [Scalar] Value at Risk of x
-  #   y_max  : [scalar] Specifying the max bound for y-axis
+  #   result      : [list] containing the VaR, the ES and the P&L to plot
+  #   model_name  : [string] of the title of the model used
   
   #  OUTPUTS
   #   NONE
+
   
-  #  NOTE
-  #   o Limits are set for the x axis (outliers may not appear in graph)
+  # Data preparation
+  n_breaks     <- round(10 * log(length(result$PL)))
+  df           <- data.frame(result$PL)
+  colnames(df) <- "PL"
   
-  # Histogram
-  windowsFonts(A = windowsFont("Times New Roman"))
-  n_breaks = round(10 * log(length(x)))
-  chart.Histogram(R = x,
-                  breaks = n_breaks,
-                  xlab = "Profit & Loss ($)",
-                  ylim = c(0,y_max),
-                  lwd = 2,
-                  main = title,
-                  probability = TRUE,
-                  colorset = c("azure4","black"),
-                  methods = c("add.density"),
-                  family = "A")
+  plot <- ggplot(data = df) + 
+    
+    # Plot Histogram
+    geom_histogram(data = df, 
+                   aes(x = PL, y = ..density..), 
+                   fill  = "black",
+                   color = "white",
+                   alpha = 0.25,
+                   bins  =  n_breaks) +
+    
+    # Overlay density
+    geom_density(aes(x = PL, colour = "Empirical Density"),
+                 lwd = 0.9) +
+    
+    # VaR vertical line
+    geom_vline(aes(xintercept = result$VaR, 
+                   colour=paste("VaR at 95% :", substr(as.character(result$VaR), 1, 7),"$")), 
+               linetype="dashed", 
+               size = 1) +
+    
+    # ES vertical line
+    geom_vline(aes(xintercept = result$ES, 
+                   colour=paste("ES at 95% :", substr(as.character(result$ES), 1, 7),"$")), 
+               linetype="dashed", 
+               size = 1) +
+    
+    # Mean vertical line
+    geom_vline(aes(xintercept = mean(result$PL), 
+                   colour=paste("P&L Mean :", substr(as.character(mean(result$PL)), 1, 5),"$")), 
+               linetype="dashed", 
+               size = 1) +
+    
+    scale_colour_manual("Legend", values=c("gray10","green", "darkblue", "brown2")) +
+    
+    theme_minimal() +
+    
+    xlab("Profit & Loss ($)") +
+    ylab("Density") +
+    ggtitle(label = "Options portfolio P&L distribution", subtitle = model_name) +
+    
+    theme(plot.title = element_text(hjust = 0.5, face = "bold"),
+          plot.subtitle = element_text(hjust = 0.5),
+          legend.position = c(0.8, 0.7),
+          legend.background = element_rect(fill = "white"),
+          legend.title.align = 0.5,
+          legend.box = NULL)
   
-  # Graph the VaR of x
-  abline(v = VaR, 
-         col = "red", 
-         lwd = 3)
-  
-  # Graph the mean of the x
-  abline(v = mean(x), 
-         col = "purple", 
-         lwd = 3)
-  
-  # Legend
-  char_VaR   <- substr(as.character(VaR), 1, 7)
-  char_mean <- substr(as.character(mean(x)), 1, 5)
-  legend(x = "topright", 
-         legend = c("Empirical Density Curve",paste("VaR at 95% =",char_VaR," $"), paste("P&L mean =", char_mean, "$")), 
-         fill = c("black", "red","purple"), 
-         cex = 0.8)
-  
+  # Output
+  plot
 }
 
 
-f_png_save <- function(x, title, VaR, y_max, file_name) {
+f_png_save <- function(result, model_name, file_name) {
   
   ### Plots the density function of x and saves it in the Output folder
   
   #  INPUTS
-  #   x         : [vector] (T x 1) of data to plot
-  #   title     : [string] of the title of the graph
-  #   VaR       : [Scalar] Value at Risk of x
-  #   y_max     : [scalar] Specifying the max bound for y-axis
+  #   result      : [list] containing the VaR, the ES and the P&L to plot
+  #   model_name  : [string] of the title of the model used
   #   file_name : [string] of the name of the .png file
   
   #  OUTPUTS
   #   NONE
   
   png(file = here("Output", paste(file_name, ".png", sep = "")))
-  f_plot_pdf(x, title, VaR, y_max)
+  print(f_plot_pdf(result, model_name))
   dev.off()
   
 }
---
+++ Function/f_price_simul_surface.R
@@ -0,0 +1,146 @@
+
+f_price_simul_vol <- function(alpha, last_vol, price_simul) {
+  
+  ### Function that adjusts the values from a prediction of the VIX with
+  ### with a parametric volatility surface
+  
+  #  INPUTS
+  #   alpha       : [vector] (4 x 1) of the values of the parameters for the 
+  #                 volatility surface
+  #   last_vol    : The value of the VIX now (last known value of the VIX)
+  #   price_simul : [matrix] (n_simul x 2) of all the simulated prices (for 
+  #                 index and vol) from one of the previous methods
+  
+  #  OUTPUTS
+  #   price_simul : [matrix] (n_simul x 2) of all the simulated prices (for 
+  #                 index and vol) with an adjustment made for the volatility
+  
+  
+  vol_ATM_param   <- alpha[1] + alpha[4]
+  delta_vol_param <- as.numeric(last_vol - vol_ATM_param)
+  
+  price_simul[,2] <- price_simul[,2] - delta_vol_param
+  
+  price_simul
+}
+
+
+f_opt_alpha <- function(option_info) {
+  
+  ### Function that finds the parameters (alpha) in order to minimize the
+  ### objective function. Only OTM puts and calls are used for the 
+  ### optimization.
+  
+  #  INPUTS
+  #   option_info : [matrix] (T x 4) of the information about both puts
+  #                 and calls. In order, the columns are the strike, the
+  #                 time to maturity in years, the IV and the moneyness (K/S)
+  
+  #  OUTPUTS
+  #   alpha : [vector] (4 x 1) of the values of the parameters
+  
+  # Optimize
+  alpha0 <- rep(0.1, 4)
+  alpha  <- optim(par = alpha0,
+                  fn  = f_objective,
+                  option_info = option_info)$par
+  alpha
+}
+
+
+f_objective <- function(alpha, option_info) {
+  
+  ### Function that defines the objective function to minimize
+  
+  #  INPUTS
+  #   alpha       : [vector] (4 x 1) of the values of the parameters
+  #   option_info : [matrix] (T x 4) of the information about both puts
+  #                 and calls. In order, the columns are the strike, the
+  #                 time to maturity in years, the IV and the moneyness (K/S)
+  
+  #  OUTPUTS
+  #   obj : [scalar] Value of the objective function
+  
+  # Market volatility
+  vol_mkt   <- option_info[,3] 
+  
+  # Parametric volatility
+  vol_param <- f_vol_param(alpha, option_info)
+  
+  # Objective function
+  obj <- sum(abs(vol_mkt - vol_param))
+  if (!is.finite(obj)) {
+    nll <- 1e10
+  }
+  
+  obj
+  
+}
+
+
+f_vol_param <- function(alpha, option_info) {
+  
+  ### Function that computes the parametric volatility
+  
+  #  INPUTS
+  #   alpha       : [vector] (4 x 1) of the values of the parameters
+  #   option_info : [matrix] (T x 4) of the information about both puts
+  #                 and calls. In order, the columns are the strike, the
+  #                 time to maturity in years, the IV and the moneyness (K/S)
+  
+  #  OUTPUTS
+  #   vol_param : [vector] (T x 1) of the computed volatility
+  
+  alpha_1 <- alpha[1]
+  alpha_2 <- alpha[2]
+  alpha_3 <- alpha[3]
+  alpha_4 <- alpha[4]
+  
+  tau <- option_info[,2]
+  m   <- option_info[,4]
+  
+  # Compute parametric volatility
+  vol_param <- alpha_1 + alpha_2 * (m-1)^2 + alpha_3 * (m-1)^3 + alpha_4 * sqrt(tau)
+  vol_param
+  
+}
+
+
+f_option_info <- function(call_info, put_info, last_price) {
+  
+  ### Function that collects the information about the puts and the calls
+  ### which will be needed in the construction of the volatility surface
+  
+  #  INPUTS
+  #   call_info  : [matrix] (T x 3) of the information about calls. In order, 
+  #                the columns are the strike, the time to maturity in years 
+  #                and the implied volatility (IV)
+  #   put_info   : [matrix] (T x 3) of the information about calls. In order, 
+  #                the columns are the strike, the time to maturity in years 
+  #                and the implied volatility (IV)
+  #   last_price : [scalar] Price for which the information is given
+  
+  #  OUTPUTS
+  #   option_info : [matrix] (T x 4) of the information about both puts
+  #                 and calls. In order, the columns are the strike, the
+  #                 time to maturity in years, the IV and the moneyness (K/S)
+  #  NOTE
+  #   o Since the volatility surface will be calibrated with OTM puts and 
+  #     calls, only those option will be included in the output option_info
+  
+  # Add the moneyness to the information
+  last_price  <- as.numeric(last_price)
+  m_call      <- call_info[,1] / as.numeric(last_price)
+  m_put       <- put_info[,1] / as.numeric(last_price)
+  
+  call_info <- cbind(call_info, m_call)
+  put_info  <- cbind(put_info, m_put)
+  
+  # Get OTM puts (m<1) and calls (m>1) 
+  call_info <- call_info[call_info[,4]>=1,]
+  put_info  <- put_info[put_info[,4]<=1,]
+  
+  # Combine OTM call information and put information
+  option_info <- rbind(call_info, put_info)
+  option_info
+}
